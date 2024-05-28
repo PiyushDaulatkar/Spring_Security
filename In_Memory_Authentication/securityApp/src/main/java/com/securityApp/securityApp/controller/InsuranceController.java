e51c0119ceb8caff48af102b88c405c8454c8741
Piyush Daulatkar <piyushdaulatkar03@gmail.com>
2024-05-28 23:23:12 +0530

    create Insurance entity

---
+++ In_Memory_Authentication/securityApp/src/main/java/com/securityApp/securityApp/controller/InsuranceController.java
@@ -0,0 +1,41 @@
+package com.securityApp.securityApp.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.securityApp.securityApp.entity.Insurance;
+
+@RestController
+public class InsuranceController {
+
+	private List<Insurance> insurances = new ArrayList<Insurance>();
+	
+	@GetMapping("/")
+	public String welcome() {
+		return ("<h1>Welcome</h1>");
+	}
+	
+	@GetMapping("/insurances")
+	public List<Insurance> getAllInsurances(){
+		
+		Insurance insurance1 = new Insurance();
+		insurance1.setId(1);
+		insurance1.setName("Car Insurance");
+		insurance1.setPremium(250);
+		
+		Insurance insurance2 = new Insurance();
+		insurance2.setId(2);
+		insurance2.setName("Health Insurance");
+		insurance2.setPremium(450);
+		
+		insurances.add(insurance1);
+		insurances.add(insurance2);
+		
+		return insurances;
+	}
+	
+	public 
+}
---
+++ In_Memory_Authentication/securityApp/src/main/java/com/securityApp/securityApp/entity/Insurance.java
@@ -0,0 +1,27 @@
+package com.securityApp.securityApp.entity;
+
+public class Insurance {
+	private long id;
+	private String name;
+	private int premium;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPremium() {
+		return premium;
+	}
+	public void setPremium(int premium) {
+		this.premium = premium;
+	}
+	
+	
+}
